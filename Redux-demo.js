323631f257009cf0c533e3b139aae76b123159e6
Esha Gautam <eshagautam0@gmail.com>
2024-03-04 22:00:45 +0530

    Action types

--- Redux-demo.js
+++ Redux-demo.js
@@ -1,11 +1,11 @@
 const redux = require('redux')
 
 const counterReducer =(state={counter:0},action)=>{
-    if(action.type==='increment'){
-    return {counter: state.counter + 1};
+    if(action.type==='incrementby2'){
+    return {counter: state.counter + 2};
     }
-   else if(action.type==='decrement'){
-    return {counter: state.counter -1};
+   else if(action.type==='decrementby2'){
+    return {counter: state.counter -2};
    }
       
 return state
@@ -20,6 +20,6 @@ console.log(latestState)
 
 store.subscribe(counterSubscribe)
 
-store.dispatch({type:'increment'})
-store.dispatch({type:'decrement'})
+// store.dispatch({type:'incrementby2'})
+store.dispatch({type:'decrementby2'})
 
